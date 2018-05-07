830fadd480313bce428df22ab67653ea6c9bc652
Evert <evert.borghgraef@thinkcore.be>
2018-05-07 21:33:06 +0200

    Introduce while loop in run + formatting to Clang Format

--- server/src/main.c
+++ server/src/main.c
@@ -1,7 +1,18 @@
+#include <signal.h>
+#include <stdbool.h>
+
 #include "controller.h"
+
+void stop_program()
+{
+    controller_stop();
+    return;
+}
 
 int main(void)
 {
+    signal(SIGINT, stop_program);
+
     controller_init();
     controller_run();
     controller_cleanup();
