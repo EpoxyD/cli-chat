47c3d2fc1498abec3febc898910696d62ba14cf2
Evert <evert.borghgraef@thinkcore.be>
2018-05-07 19:57:35 +0200

    Add events to eventloop

--- server/src/controller.c
+++ server/src/controller.c
@@ -5,26 +5,30 @@
 #include <stdio.h>
 #include <sys/socket.h>
 
+static int epoll_fd;
+static int main_socket;
+
 void controller_init()
 {
-    int socket, error, polling_fd;
-    socket = connection_create_socket(AF_INET, SOCK_STREAM);
-    fprintf(stdout, "Created socket %d\n", socket);
-    error = connection_bind_socket(socket);
-    fprintf(stdout, "Bind socket %d. Status %d\n", socket, error);
-    error = connection_listen_socket(socket);
-    fprintf(stdout, "Listen on socket %d. Status %d\n", socket, error);
+    main_socket = connection_create_socket(AF_INET, SOCK_STREAM);
+    fprintf(stdout, "Created   socket %d\n", main_socket);
+    int error = connection_bind_socket(main_socket);
+    fprintf(stdout, "Bind   on socket %d. Status %d\n", main_socket, error);
+    error = connection_listen_socket(main_socket);
+    fprintf(stdout, "Listen on socket %d. Status %d\n", main_socket, error);
     
-    polling_fd = eventloop_create();
-    fprintf(stdout, "Created eventloop %d\n", polling_fd);
-    error = eventloop_close(polling_fd);
-    fprintf(stdout, "Closed eventloop %d. Status %d\n", polling_fd, error);
+    epoll_fd = eventloop_create_fd();
+    fprintf(stdout, "Created       eventloop %d\n", epoll_fd);
 }
 
 void controller_run()
 {
+    int error = eventloop_add_event(epoll_fd, main_socket);
+    fprintf(stdout, "Added fd %d to eventloop %d. Status %d\n", main_socket, epoll_fd, error);
 }
 
 void controller_cleanup()
 {
+    int error = eventloop_close(epoll_fd);
+    fprintf(stdout, "Closed        eventloop %d. Status %d\n", epoll_fd, error);
 }
